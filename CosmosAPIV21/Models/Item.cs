ef300c4fd3ad28031bef8b4b9148180e4c21445f
Niharika Vadlamudi <niharikavadlamudi@Niharikas-MacBook-Pro.local>
2021-05-23 02:15:22 +0530

    Simpler Version of WEB APIs

---
+++ CosmosAPIV21/Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CosmosAPIV21.Models
+{
+    public class Item
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        // More Additions !
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "PhoneNumber")]
+        //public int PhoneNumber { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "Location")]
+        //public string Location { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "ProductType")]
+        //public string ProductType { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "AccountType")]
+        //public string AccountType { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "AssignedLicense")]
+        //public int AssignedLicense { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "CallingType")]
+        //public string CallingType { get; set; }
+        //[Newtonsoft.Json.JsonProperty(PropertyName = "AudioConferencing")]
+        //public string AudioConferencing { get; set; }
+    }
+}
